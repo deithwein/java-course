3166cf3cee499769470cb02c7357945903244bcc
Anton Khudiakov <antonyvkh@gmail.com>
2022-09-30 00:14:44 +0400

    another commit

---
+++ src/Lesson2/MyClass.java
@@ -0,0 +1,12 @@
+package Lesson2;
+
+public class MyClass {
+
+    public static void first() {
+        System.out.println("Это статический метод");
+    }
+
+    public void second() {
+        System.out.println("Это НЕ статический метод");
+    }
+}
---
+++ src/Lesson2/Test.java
@@ -0,0 +1,9 @@
+package Lesson2;
+
+public class Test {
+    public static void main(String[] args) {
+        MyClass c1 = new MyClass();
+        c1.second();
+        MyClass.first();
+    }
+}
