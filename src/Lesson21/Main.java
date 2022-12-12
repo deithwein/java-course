82312d3fe20e37a7ee62ff5a276d8a0601ce000b
Anton Khudiakov <antonyvkh@gmail.com>
2022-12-13 00:51:08 +0400

    regex task

--- src/Lesson21/Main.java
+++ src/Lesson21/Main.java
@@ -1,10 +1,40 @@
 package Lesson21;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(test());
-        System.out.println("----------");
-        System.out.println(test2());
+        String s = "www.telran.de";
+        String s1 = "https://www.google.com";
+
+        Pattern p = Pattern.compile("((http://)|(https://))?www\\.[a-z0-9]+\\.[a-z]{2,3}");
+
+        System.out.println(p.matcher(s).matches());         // true
+        System.out.println(p.matcher(s1).matches());        // true
+
+        String st1 = "telran.de";
+        String st2 = "My telephone number is +10000000000";
+
+        writePositionsString(st1);
+        writePositionsString(st2);
+
+    }
+
+    static void writePositionsString(String s) {
+        Pattern p = Pattern.compile("tel");
+        Matcher m = p.matcher(s);
+        int start, end;
+        if (m.find()) {
+            start = m.start();
+            end = m.end();
+        }
+        else {
+            start = -1;
+            end = -1;
+        }
+        //Вычислить start и end для первой и второй строк
+        System.out.println("For the string " + s + " pattern found at positions from " + start + " to " + end);
     }
 
     public static String test2() {
